1f8be7f765c128f077b305430437fdbed7f0a04c
dmitry <kosenkodmitryv@gmail.com>
2014-07-24 11:10:11 +0400

    Add footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def example(options={}, &block)
     out = render :partial => 'home/header', :locals => {:options => options}
     out << capture(&block)
-    #out << (render :partial => 'examples/footer', :locals => {:options => options})
+    #out << (render :partial => 'home/footer', :locals => {:options => options})
     out
   end
 
