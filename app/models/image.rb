5f4f49849acd31b7c1eb713b3dbdcb6ae6740027
dmitry <kosenkodmitryv@gmail.com>
2014-07-29 14:51:01 +0400

    Display missing image (Shop List)

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
 
   #has_attached_file :path, :styles => { :large => "800x800", :medium => "400x400>", :small => "200x200>" }
   #attr_accessible :path, :urls
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/thumb/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def image_url_thumb
