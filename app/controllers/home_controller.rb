f7110d05ad18b67b21fbb66f4f00044423f4f517
dmitry <kosenkodmitryv@gmail.com>
2014-07-31 20:50:06 +0400

    Remove unused images. Add Seed Data for Services and Banners.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -88,12 +88,13 @@ class HomeController < ApplicationController
 
     #@tcats = CategoryItem.where(parent_id:0).order(name: :asc)#.paginate(:page => params[:page], :per_page => 10) #Shop.all#[0..3]
     @tcats = CategoryItem.all.order(name: :asc)
+    if (!@tcats.blank?)
+      tcid = (tcid > 0) ? tcid : @tcats.first.id if
+      shop_id = (shop_id > 0) ? shop_id : @tcats.first.shops.first.id if @tcats.first.shops.first
 
-    tcid = (tcid > 0) ? tcid : @tcats.first.id
-    shop_id = (shop_id > 0) ? shop_id : @tcats.first.shops.first.id if @tcats.first.shops.first
-
-    @shops = @tcats.find( tcid ).shops.order(favorite: :desc, rated: :desc, name: :asc).paginate(:page => params[:page], :per_page => 10)
-    @shop = @shops.find( shop_id ) if shop_id > 0
+      @shops = @tcats.find( tcid ).shops.order(favorite: :desc, rated: :desc, name: :asc).paginate(:page => params[:page], :per_page => 10)
+      @shop = @shops.find( shop_id ) if shop_id > 0
+    end
     #@shop = Shop.find( shop_id )
     #@shops = CategoryItem.find( shop_id ).shops.order(favorite: :desc, rated: :desc, name: :asc).paginate(:page => params[:page], :per_page => 10)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,39 @@
 #ci = ContactItem.create(name:'Website (Official)', value:'www.ex.com', shop:s, contact_type:ct_url)
 #ci = ContactItem.create(name:'Facebook', value:'www.facebook.com/ex', shop:s, contact_type:ct_url)
 
-AdminUser.create!(:email => 'spree@example.com', :password => 'spree123', :password_confirmation => 'spree123')
+
+#AdminUser.create!(:email => 'spree@example.com', :password => 'spree123', :password_confirmation => 'spree123')
+AdminUser.create!(:email => 'shopomob@shopomob.ru', :password => 'shopomobpass', :password_confirmation => 'shopomobpass')
 
 
 require 'csv'
 path_to_app = File.dirname(__FILE__)+'/csv/'
 path_to_img = File.dirname(__FILE__)+'/images/'
 
+
+file_path = "#{path_to_app}services.csv"
+puts file_path
+CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
+  item = Service.create( order_id: row['order_id'].to_i, title: row['title'], text: row['text'], url: row['url'])
+  if (!row['image'].blank?)
+    img_path = path_to_img + row['image']
+    if (File.exists?(img_path))
+      item.image = Image.create(:image=>File.open(img_path))
+    end
+  end
+end
+
+file_path = "#{path_to_app}banners.csv"
+puts file_path
+CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
+  item = Banner.create(shop_id: row['shop_id'].to_i, title: row['title'], url: row['url'])
+  if (!row['image'].blank?)
+    img_path = path_to_img + row['image']
+    if (File.exists?(img_path))
+      item.image = Image.create(:image=>File.open(img_path))
+    end
+  end
+end
 
 file_path = "#{path_to_app}tcats.csv"
 puts file_path
@@ -63,7 +89,7 @@ puts file_path
 CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
   #puts "#{row['name']} #{row['address']} "
   pid = row['parent_id'].to_i
-  shop = Shop.create(_id: row['_id'].to_i, name: row['name'], parent_id: pid, postal_code: row['address_index'].to_i, address: row['address'], time_work: row['time'], email: row['email'], www: row['www'], favorite: row['favorite'].to_i, rated: 0) #:
+  shop = Shop.create(_id: row['_id'].to_i, name: row['name'], parent_id: pid, postal_code: row['address_index'].to_i, address: row['address'], time_work: row['time'], email: row['email'], www: row['www'], favorite: row['favorite'].to_i, rated: 0, tags: row['tags'], description: row['description'] ) #:
   if pid == 0
     #c = Category.create(name: row['name'] )
     puts "parent shop: #{row['name']}"
@@ -72,6 +98,30 @@ CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
     puts "shop: #{row['name']}"
   end
 end
+
+
+
+ct_mail = ContactType.create(name:'email', value:'Email Address')
+ct_url = ContactType.create(name:'url', value:'Website or other link')
+ct_phone = ContactType.create(name:'phone', value:'Phone Number')
+file_path = "#{path_to_app}phones.csv"
+puts file_path
+#_id	id_org	ord	phone	owner	owner_fio
+CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
+  s = Shop.find_by(_id: row['id_org'].to_i)
+  if s
+    #ci = ContactItem.find_by(_id: row['id_trubr'].to_i)
+    ci = ContactItem.create(fio: row['owner_fio'], department:row['owner'], value:row['phone'], shop:s, contact_type:ct_phone)
+
+    if ci
+      #c.category_item_ids.append( ci.id )
+      s.contact_items.append( ci )
+      s.save
+      puts "#{s.name} | #{ci.department} | #{ci.value} "
+    end
+  end
+end
+
 
 =begin
 file_path = "#{path_to_app}company_cats.csv"
@@ -105,25 +155,3 @@ CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
   end
 end
 =end
-
-
-ct_mail = ContactType.create(name:'email', value:'Email Address')
-ct_url = ContactType.create(name:'url', value:'Website or other link')
-ct_phone = ContactType.create(name:'phone', value:'Phone Number')
-file_path = "#{path_to_app}phones.csv"
-puts file_path
-#_id	id_org	ord	phone	owner	owner_fio
-CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
-  s = Shop.find_by(_id: row['id_org'].to_i)
-  if s
-    #ci = ContactItem.find_by(_id: row['id_trubr'].to_i)
-    ci = ContactItem.create(fio: row['owner_fio'], department:row['owner'], value:row['phone'], shop:s, contact_type:ct_phone)
-
-    if ci
-      #c.category_item_ids.append( ci.id )
-      s.contact_items.append( ci )
-      s.save
-      puts "#{s.name} | #{ci.department} | #{ci.value} "
-    end
-  end
-end
